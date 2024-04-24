81d47bc4df41812e8a8cf783e71dd2394f46133e
Chidsanuphong Pengchai <51116872+pcismyname@users.noreply.github.com>
2024-04-24 18:28:32 +0700

    change layout

--- front-end/src/routes/+layout.svelte
+++ front-end/src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 </script>
 
 <div data-theme="mytheme">
+
 	<Header />
 
 	<main>
--- front-end/src/routes/top-pick/+page.svelte
+++ front-end/src/routes/top-pick/+page.svelte
@@ -3,36 +3,51 @@
   import etheruem_img from "$lib/images/etheruem.png";
   import usdt_img from "$lib/images/usdt.png";
   import xrp_img from "$lib/images/xrp.png";
-  
+
   import Card from "../Card.svelte";
 
   //this shoud get data from server
   const coins = [
-    { coinId: "btc", detail :{image: bitcoin_img, name: "Bitcoin", price: "$40,000" }},
-    { coinId: "eth", detail :{image: etheruem_img, name: "etheruem", price: "$40,000" }},
-    { coinId: "usdt", detail :{image: usdt_img, name: "Bitcoin", price: "$40,000" }},
-    { coinId: "xrp", detail :{image: xrp_img, name: "Bitcoin", price: "$40,000" }},
+    {
+      coinId: "btc",
+      detail: { image: bitcoin_img, name: "Bitcoin", price: "$40,000" },
+    },
+    {
+      coinId: "eth",
+      detail: { image: etheruem_img, name: "etheruem", price: "$40,000" },
+    },
+    {
+      coinId: "usdt",
+      detail: { image: usdt_img, name: "Bitcoin", price: "$40,000" },
+    },
+    {
+      coinId: "xrp",
+      detail: { image: xrp_img, name: "Bitcoin", price: "$40,000" },
+    },
   ];
 </script>
 
-<body class="flex flex-col h-dvh ">
-<div class="bg-primary pt-5  h-screen">
-  <p class="text-4xl mb-10 font-bold text-base-100 text-center">
-    Top Pick
-  </p>
-  <div class="flex flex-wrap justify-center items-center gap-4">
-    {#each coins as coin}
-    <Card
-      coinId={coin.coinId}
-      image={coin.detail.image}
-      name={coin.detail.name}
-      price={coin.detail.price}
-    />
-  {/each}
+<!-- <body class="flex flex-col h-dvh"> -->
+  <div class="bg-primary pt-5 h-full">
+    <p class="text-4xl mb-10 font-bold text-base-100 text-center">Top Pick</p>
+    <div class="flex flex-wrap h-full bg-primary justify-center items-center gap-4">
+      {#each coins as coin}
+        <Card
+          coinId={coin.coinId}
+          image={coin.detail.image}
+          name={coin.detail.name}
+          price={coin.detail.price}
+        />
+      {/each}
+      {#each coins as coin}
+        <Card
+          coinId={coin.coinId}
+          image={coin.detail.image}
+          name={coin.detail.name}
+          price={coin.detail.price}
+        />
+      {/each}
+    </div>
   </div>
-</div>
-</body>
-<style lang="postcss">
-  body {
-  }
-</style>
+<!-- </body> -->
+
