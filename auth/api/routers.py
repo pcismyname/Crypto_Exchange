ee1498841e610392f80dfdbaa25542802a9447c6
Chidsanuphong Pengchai <51116872+pcismyname@users.noreply.github.com>
2024-04-25 15:26:45 +0700

    remove file

--- auth/api/routers.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter
-from api import  login
-
-api_router = APIRouter()
-api_router.include_router(login.router, prefix="/login", tags=["login"])
--- common/utilities.py
+++
@@ -1,5 +0,0 @@
-# common/utilities.py
-
-def format_date(date):
-    """Format date as a string."""
-    return date.strftime("%Y-%m-%d %H:%M:%S")
